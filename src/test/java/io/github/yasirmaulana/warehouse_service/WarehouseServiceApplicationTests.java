c7711df2d93aae1ca4a08b05f4f8ea2b0b382042
yasirmaulana <yasir.maulana@gmail.com>
2024-10-07 16:18:56 +0700

    first commit

---
+++ src/test/java/io/github/yasirmaulana/warehouse_service/WarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yasirmaulana.warehouse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
